e40e7b0dd988582189bda4da6db3e70ccc1dc311
u4bi <u4bi@u4bi.com>
2016-11-26 04:16:39 +0000

    w3s game tuto add a component

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 /*index js*/
 
-function init(){
-  myGameArea.start();
-};
+var myGamePiece;
 
 var myGameArea = {
   canvas : document.createElement("canvas"),
@@ -14,3 +12,19 @@ var myGameArea = {
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
 };
+
+function init(){
+  myGameArea.start();
+  myGamePiece = new component(30,30, "red", 10, 120);
+};
+
+function component(width, height, color, x, y){
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  
+  var ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
