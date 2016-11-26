a8add3d486858059259d8ba337b2d0ac271bb0bc
u4bi <u4bi@u4bi.com>
2016-11-26 05:14:26 +0000

    w3s game tuto - many components (red, blue, yellow piece)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 /*index js*/
 
 var myGamePiece;
+var redGamePiece, blueGamePiece, yellowGamePiece;
 
 var myGameArea = {
   canvas : document.createElement("canvas"),
@@ -19,6 +20,11 @@ var myGameArea = {
 
 function init(){
   myGameArea.start();
+  
+  redGamePiece = new component(75,75, "red", 10, 10);
+  blueGamePiece = new component(75,75, "yellow", 10, 100);
+  yellowGamePiece = new component(75,75, "blue", 10, 200);
+  
   myGamePiece = new component(10,140, 'rgba(0, 0, 255, 0.5)', 2, 2); /* x, y */
 };
 
@@ -38,4 +44,8 @@ function updateGameArea(){
   myGameArea.clear(); /* 지우고 다시 그리고하기 때문에 만약 clear을 주석처리하면 지나온 흔적을 남겨면서 그려짐*/
   myGamePiece.x += 1;
   myGamePiece.update();
+  
+  redGamePiece.update();
+  blueGamePiece.update();
+  yellowGamePiece.update();
 }
