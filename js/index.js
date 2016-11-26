747d48d8e9e378c2b41cfdbfc0880fd284435822
u4bi <u4bi@u4bi.com>
2016-11-26 03:52:31 +0000

    w3s game tuto - createElement canvas

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 /*index js*/
 
-init();
-function init(){};
+function init(){
+  myGameArea.start();
+};
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+    start: function(){
+      this.canvas.width = 480;
+      this.canvas.height = 270;
+      this.context = this.canvas.getContext("2d");
+    
+      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+};
