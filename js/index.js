2910113ff656d6df3d5839bca862232c70896853
u4bi <u4bi@u4bi.com>
2016-11-26 06:07:58 +0000

    w3s game tuto - keyvoard as controller

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ var myGameArea = {
     
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
       this.interval = setInterval(updateGameArea, 20);
+      windwo.addEventListener('', function(e){ } );
+      windwo.addEventListener('', function(e){ } );
     },
     clear: function(){
       this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
@@ -61,7 +63,6 @@ function updateGameArea(){
   blueGamePiece.x +=2;
   yellowGamePiece.x +=2;
 }
-
 /* move type
    @ type : up 0/down 1/left 2/right 3/
 */
