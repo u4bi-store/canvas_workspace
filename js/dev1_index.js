d8e8a42255708292b957767c77660cd2b439cee4
u4bi <u4bi@u4bi.com>
2016-11-27 02:57:48 +0000

    player init

--- js/dev1_index.js
+++ js/dev1_index.js
@@ -1,14 +1,29 @@
 /*dev1_index js*/
+
+var player;
+
 function startGame() {
   area.start();
+  player = new component(50,30, 'red', 2, 2);
 }
 
 var area = {
-  canvas : document.createElement("canvas"),
+  canvas : document.createElement('canvas'),
   start : function() {
     this.canvas.width = 600;
     this.canvas.height = 360;
-    this.context = this.canvas.getContext("2d");
+    this.context = this.canvas.getContext('2d');
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
   }
 };
+
+function component(width, height, color, x, y){
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  
+  var ctx = area.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
