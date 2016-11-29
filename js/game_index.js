5ff2a871b29cc82fe6f818c4df171f315cd21f5c
u4bi <u4bi@u4bi.com>
2016-11-29 11:09:18 +0000

    Mozilla canvas game tuto - defining a drawing loop 2

--- js/game_index.js
+++ js/game_index.js
@@ -1,13 +1,22 @@
 /* game_index js*/
 
 var canvas,ctx;
+var x, y;
 
 function init(){
   canvas = document.getElementById('myCanvas');
   ctx = canvas.getContext('2d');
   
+  x = canvas.width/2;
+  y = canvas.height-30;
+  
   setInterval(draw, 10);
 }
 
 function draw(){
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();  
 }
