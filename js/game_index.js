bb3f01204b735aeb5fea2abdb355cefbc0507b55
u4bi <u4bi@u4bi.com>
2016-11-29 12:26:26 +0000

    Mozilla canvas game tuto - cleaning up our code

--- js/game_index.js
+++ js/game_index.js
@@ -17,13 +17,17 @@ function init(){
   setInterval(draw, 10);
 }
 
-function draw(){
-  ctx.clearRect(0,0, canvas.width, canvas.height);
+function drawBall(){
   ctx.beginPath();
   ctx.arc(x, y, 10, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
-  ctx.closePath();
+  ctx.closePath();  
+}
+
+function draw(){
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  drawBall();
   x += dx;
   y += dy;
 }
